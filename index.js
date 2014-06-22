d6e5a2dc674be93abf16735e9c8b21eef5212d95
Michael Schwartz <mykes@sportstwo.com>
2014-06-22 09:10:00 -0700

    debug

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ importClass(com.mongodb.jvm.BSON);
 
 // JSON converstion with support for MongoDB extended notation
 JSON = com.threecrickets.jvm.json.JSON;
-//JSON.implementation = new Packages.com.mongodb.jvm.MongoRhinoJsonImplementation();
+JSON.implementation = new Packages.com.mongodb.jvm.rhino.MongoRhinoJsonImplementation();
 
 decaf.extend(exports, {
     MongoDB: require('lib/MongoDB').MongoDB,
